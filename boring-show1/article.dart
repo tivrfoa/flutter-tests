dcb95ca1387e2c58cd0178676b8a18ceb6f70e2d
le <lescoutinhovr@gmail.com>
2018-08-13 18:43:38 -0300

    Article class

---
+++ boring-show1/article.dart
@@ -0,0 +1,111 @@
+
+
+class Article {
+  final String text;
+  final String domain;
+  final String by;
+  final String age;
+  final int score;
+  final int commentsCount;
+
+  const Article({
+    this.text,
+    this.domain,
+    this.by,
+    this.age,
+    this.score,
+    this.commentsCount
+  });
+
+}
+
+final articles = [
+  new Article(
+    text: "First Article",
+    domain: "wiley.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text: "Second BMW says electric car mass production not viable until 2020",
+    domain: "reuters.com",
+    by: "Mononokay",
+    age: "2 hours",
+    score: 81,
+    commentsCount: 128,
+  ),
+  new Article(
+    text: "3. Circular Shock Acoustic Waves in Ionosphere",
+    domain: "wiley.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text: "4. BMW says electric car mass production not viable until 2020",
+    domain: "reuters.com",
+    by: "Mononokay",
+    age: "2 hours",
+    score: 81,
+    commentsCount: 128,
+  ),
+  new Article(
+    text: "5. Circular Shock Acoustic Waves in Ionosphere",
+    domain: "wiley.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text: "6. BMW says electric car mass production not viable until 2020",
+    domain: "reuters.com",
+    by: "Mononokay",
+    age: "2 hours",
+    score: 81,
+    commentsCount: 128,
+  ),
+  new Article(
+    text: "7. Circular Shock Acoustic Waves in Ionosphere",
+    domain: "wiley.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text: "8. BMW says electric car mass production not viable until 2020",
+    domain: "reuters.com",
+    by: "Mononokay",
+    age: "2 hours",
+    score: 81,
+    commentsCount: 128,
+  ),
+  new Article(
+    text: "9. Circular Shock Acoustic Waves in Ionosphere",
+    domain: "wiley.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text: "Mbappe x Neymar",
+    domain: "psg.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text: "FINAL ARTICLE",
+    domain: "leandro.com",
+    by: "leandro",
+    age: "1/2 hours",
+    score: 81,
+    commentsCount: 128,
+  ),
+];
